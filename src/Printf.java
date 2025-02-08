8990b037ebbacfeab28d3aa37b74844d9fe5238d
Augustine0077 <135913304+Augustine0077@users.noreply.github.com>
2025-02-08 16:36:17 +0530

    init commit

---
+++ src/Printf.java
@@ -0,0 +1,7 @@
+public class Printf {
+    public static void main(String[] args) {
+        String letter = "Augustine";
+        Character First_letter = 'Z';
+        System.out.printf("hello %.1s", letter);
+    }
+}
