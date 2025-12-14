5b42521d16f4219445d4009f35bf65a527fbafc3
Augustine Shaji <augustineshaji007@gmail.com>
2025-12-14 23:59:24 +0530

    user input various data type in java

---
+++ InputUser.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class InputUser {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter your name :");
+    String name = sc.nextLine();
+
+    System.out.println("Enter your age :");
+    int age = sc.nextInt();
+    
+    System.out.println("Enter your GPA :");
+    Double gpa = sc.nextDouble();
+
+    System.out.println("Are you a studnet (true / false)");
+    boolean student = sc.nextBoolean();
+
+   
+
+    System.out.println("Your name is "+ name);
+    System.out.println("Your age is "+age);
+    System.out.println("Your GPA is : "+gpa);
+
+    if(student){
+        System.out.println("You can enter to the class");
+    }
+    else{
+        System.out.println("You can't enter into the class");
+    }
+
+    }
+}
