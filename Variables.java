336ed9e4beca85ba814f6b661368347f9cc719db
Augustine Shaji <augustineshaji007@gmail.com>
2025-12-14 15:05:55 +0530

    variables in java

--- Variables.java
+++ Variables.java
@@ -12,6 +12,29 @@ public class Variables {
     double price = 19.56;
     System.out.println("\n The price of banana is " + price);
 
+    //char
+    char mark = 'A';
+    char symbol = '!';
+    char currency = '$';
+    System.out.println("you got mark in last exam " + mark);
+    System.out.println("tthe symbol is " + symbol);
+    System.out.println("The currency is "+currency);
+
+    
+    //boolean
+
+    boolean isStudent = true;
+    boolean isPassed = false;
+    boolean isOnline = true;
+
+    System.out.println(isStudent);
+    System.out.println(isPassed);
+    System.out.println(isOnline);
+
+    //String
+    String name = "Augustine Shaji";
+    System.out.print("Hello "+name);
+
     }
     
 }
