f594d4d0a481f660216929e54bb565137ed510e2
Augustine Shaji <augustineshaji007@gmail.com>
2025-12-14 12:50:18 +0530

    varibales learning

---
+++ Variables.java
@@ -0,0 +1,17 @@
+public class Variables {
+    public static void main(String[]args){
+
+    //Decleration
+    
+    //int
+    int age = 23;
+    System.out.println("Your age is  "+ age);
+
+
+    //Double
+    double price = 19.56;
+    System.out.println("\n The price of banana is " + price);
+
+    }
+    
+}
