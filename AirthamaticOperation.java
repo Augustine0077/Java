80beb2c2fb2fb03e78899dfa784484cd10797e7f
Augustine Shaji <augustineshaji007@gmail.com>
2025-12-15 16:08:47 +0530

    Airthmatic operation and other operations in java

---
+++ AirthamaticOperation.java
@@ -0,0 +1,22 @@
+public class AirthamaticOperation {
+    public static void main(String[] args) {
+        int num1 =10;
+        int num2 =10;
+        double num3 ;
+
+
+        //num3 = num1+num2;
+        //num3 = num1*num2;
+        //num3 = num1/num2;
+        //num3 = num1%num2;
+
+        //Augumented assignments
+        //num1+=num2;
+        //num1-=num2;
+        
+
+        System.out.println(num1);
+
+    }
+    
+}
