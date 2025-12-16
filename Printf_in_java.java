a8fa645d750668f0a244808b9ab80a173d904306
Augustine Shaji <augustineshaji007@gmail.com>
2025-12-16 20:28:02 +0530

    compound intrest and printf in java

---
+++ Printf_in_java.java
@@ -0,0 +1,15 @@
+public class Printf_in_java {
+    public static void main(String[] args) 
+    {
+        
+    
+    String name = "Augustine";
+    char firstletter = 'A';
+    int age=30;
+    double height = 3.5;
+    boolean is_employeed = true;
+
+    //System.out.printf("hello %s",name);
+    System.out.printf("hello  %s and his fist caharcter is %c and his age is %d and his height is %f and he is employeed %b",name,firstletter,age,height,is_employeed);
+    }
+}
