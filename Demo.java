2ad2ebc488b72029a31b80576e0ab31f07f5dc7c
Augustine Shaji <135913304+Augustine0077@users.noreply.github.com>
2026-01-29 14:41:27 +0530

    Add files via upload

--- Demo.java
+++ Demo.java
@@ -1,5 +1,18 @@
-class Demo{
-    public static void main(String[]args){
-        System.out.println("Hello world");
+class Friend {
+    String name;
+    static int num_of_friends = 0;
+
+    Friend(String name) {
+        this.name = name;
+        num_of_friends++;
+    }
+}
+
+public class Demo {
+    public static void main(String[] args) {
+        Friend f1 = new Friend("Augustine");
+        Friend f2 = new Friend("Ambani");
+
+        System.out.println("Total friends: " + Friend.num_of_friends);
     }
 }
---
+++ sample.java
@@ -0,0 +1,5 @@
+public record sample() {
+    public static  void main(String[] args){
+    System.out.println("This is a sample record.");
+    
+}}
