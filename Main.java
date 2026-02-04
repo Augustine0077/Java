a09520941e07eedc6d9d063eae7b9ba31b0c211c
Augustine Shaji <135913304+Augustine0077@users.noreply.github.com>
2026-02-04 14:40:19 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,6 +1,37 @@
-public class Main {
-    public static void main(String[]args){
-        System.out.println("Hello world");
-        System.out.print("Hello augustine");
+package src;
+class Students {
+    private String name ;
+    private int age ;
+
+    public String Getnamee() {
+        return name;
     }
+    public void setName(String n){
+        name = n;
+    }
+
+    public int Getagee() {
+        return age;
+    }
+    public void setAge(int a){
+        age = a;
+    }
+
+
 }
+
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+        Students stud = new Students();
+       stud.setName("Augustine");
+       stud.setAge(17);
+        System.out.println(stud.Getnamee() + " : " + stud.Getagee());
+
+    }
+
+}
+
